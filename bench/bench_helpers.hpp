6d1871766d607a4ac775fef9ff5bc4b3220f5dba
Sami Hatna <sami.hatna2003@gmail.com>
2024-11-23 12:09:00 +0000

    unary matrix op benchmarks

--- bench/bench_helpers.hpp
+++ bench/bench_helpers.hpp
@@ -8,7 +8,6 @@
 #include "utils.hpp"
 
 using time_unit = std::micro;
-const std::string time_unit_str = "μs";
 
 class BenchFixture {
 public:
@@ -22,7 +21,7 @@ void benchmarkRunner(BenchFixture* benchFixture, std::string name) {
     std::cout << "\033[1;32m" << title << "\033[0m" << std::endl;
 
     constexpr int warmupRuns = 10;
-    constexpr int benchmarkRuns = 100;
+    constexpr int benchmarkRuns = 50;
 
     std::cout << "Warming up ..." << std::endl;
 
@@ -47,7 +46,7 @@ void benchmarkRunner(BenchFixture* benchFixture, std::string name) {
 
     double totalTime = std::accumulate(runTimes.begin(), runTimes.end(), 0, std::plus<double>());
     double meanTime = totalTime / benchmarkRuns;
-    std::cout << "\e[1m" <<  "Avg. execution time: " << meanTime << time_unit_str << std::endl;
+    std::cout << "\e[1m" <<  "Avg. execution time: " << (meanTime / 1000) << "ms" << std::endl;
 
     double variance = 0.0;
     for (double time : runTimes) {
@@ -55,13 +54,13 @@ void benchmarkRunner(BenchFixture* benchFixture, std::string name) {
     }
     variance /= benchmarkRuns;
     double stdDevTime = std::sqrt(variance);
-    std::cout << "\e[1m" <<  "std: " << stdDevTime << time_unit_str << std::endl;
+    std::cout << "\e[1m" <<  "std: " << (stdDevTime / 1000) << "ms" << std::endl;
 
     double minTime = *std::min_element(runTimes.begin(), runTimes.end());
-    std::cout << "\e[1m" <<  "Fastest time: " << minTime << time_unit_str << std::endl;
+    std::cout << "\e[1m" <<  "Fastest time: "  << (minTime / 1000) << "ms" << std::endl;
 
     double maxTime = *std::max_element(runTimes.begin(), runTimes.end());
-    std::cout << "\e[1m" <<  "Slowest time: " << maxTime << time_unit_str << std::endl;
+    std::cout << "\e[1m" <<  "Slowest time: "  << (maxTime / 1000) << "ms" << std::endl;
 
     std::string tail(title.length(), '=');
     std::cout << "\033[1;32m" << tail << "\033[0m" << std::endl;
--- bench/utils_bench.cpp
+++ bench/utils_bench.cpp
@@ -8,7 +8,7 @@
 #include "bench_helpers.hpp"
 #include "utils.hpp"
 
-class MatrixOpBenchFixture : public BenchFixture {
+class MatrixBinaryOpBenchFixture : public BenchFixture {
 protected:
     Vec2d<double> a;
     Vec2d<double> b;
@@ -20,7 +20,7 @@ private:
     int r1, c1, r2, c2;
     
 public:
-    MatrixOpBenchFixture(int pR1, int pC1, int pR2, int pC2) : BenchFixture(), gen(rd()), dis(0, 100) {
+    MatrixBinaryOpBenchFixture(int pR1, int pC1, int pR2, int pC2) : BenchFixture(), gen(rd()), dis(0, 100) {
         r1 = pR1;
         c1 = pC1;
         r2 = pR2;
@@ -40,18 +40,84 @@ public:
     }
 };
 
-class MatMulBenchFixture : public MatrixOpBenchFixture {
+class MatrixUnaryOpBenchFixture : public BenchFixture {
+protected:
+    Vec2d<double> a;
+
+private:
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_int_distribution<> dis;
+    int r, c;
+
 public:
-    MatMulBenchFixture(int pR1, int pC1, int pR2, int pC2) : MatrixOpBenchFixture(pR1, pC1, pR2, pC2) {}
+    MatrixUnaryOpBenchFixture(int pR, int pC) : BenchFixture(), gen(rd()), dis(0, 100) {
+        r = pR;
+        c = pC;
+    }
+
+    void setup() override {
+        a.resize(r, std::vector<double>(c));
+        for (auto& row : a) {
+            std::ranges::generate(row, [&]() { return dis(gen); });
+        }
+    }
+};
+
+class MatMulBenchFixture : public MatrixBinaryOpBenchFixture {
+public:
+    MatMulBenchFixture(int pR1, int pC1, int pR2, int pC2) : MatrixBinaryOpBenchFixture(pR1, pC1, pR2, pC2) {}
 
     void run() override {
         a * b;
     }
 };
 
+class MatDivBenchFixture : public MatrixBinaryOpBenchFixture {
+public:
+    MatDivBenchFixture(int pR1, int pC1, int pR2, int pC2) : MatrixBinaryOpBenchFixture(pR1, pC1, pR2, pC2) {}
+
+    void run() override {
+        a / b;
+    }
+};
+
+class MatTransposeBenchFixture : public MatrixUnaryOpBenchFixture {
+public:
+    MatTransposeBenchFixture(int pR, int pC) : MatrixUnaryOpBenchFixture(pR, pC) {}
+
+    void run() override {
+        transpose(a);
+    }
+};
+
 int main() {
-    MatMulBenchFixture matMulBench_50x50_50x50 {50, 50, 50, 50};
-    benchmarkRunner(&matMulBench_50x50_50x50, "matmul_50x50_50x50");
+    MatMulBenchFixture matMulBench_100x100_100x100 {100, 100, 100, 100};
+    benchmarkRunner(&matMulBench_100x100_100x100, "mat_mul_100x100_100x100");
+
+    MatMulBenchFixture matMulBench_500x500_500x500 {500, 500, 500, 500};
+    benchmarkRunner(&matMulBench_500x500_500x500, "mat_mul_500x500_500x500");
+
+    MatMulBenchFixture matMulBench_1000x1000_1000x1000 {1000, 1000, 1000, 1000};
+    benchmarkRunner(&matMulBench_1000x1000_1000x1000, "mat_mul_1000x1000_1000x1000");
+
+    MatDivBenchFixture matDivBench_100x100_100x100 {100, 100, 100, 100};
+    benchmarkRunner(&matDivBench_100x100_100x100, "mat_div_100x100_100x100");
+
+    MatDivBenchFixture matDivBench_500x500_500x500 {500, 500, 500, 500};
+    benchmarkRunner(&matDivBench_500x500_500x500, "mat_div_500x500_500x500");
+
+    MatDivBenchFixture matDivBench_1000x1000_1000x1000 {1000, 1000, 1000, 1000};
+    benchmarkRunner(&matDivBench_1000x1000_1000x1000, "mat_div_1000x1000_1000x1000");
+
+    MatTransposeBenchFixture matTransposeBench_100x100 {100, 100};
+    benchmarkRunner(&matTransposeBench_100x100, "mat_transpose_100x100");
+
+    MatTransposeBenchFixture matTransposeBench_500x500 {500, 500};
+    benchmarkRunner(&matTransposeBench_500x500, "mat_transpose_500x500");
+
+    MatTransposeBenchFixture matTransposeBench_1000x1000 {1000, 1000};
+    benchmarkRunner(&matTransposeBench_1000x1000, "mat_transpose_1000x1000");
 
     return 0;
 }
